728446d4280b1e272bed490fe7530e88c2a84fe3
utkan <utkan@LAPTOP-4886AGDL>
2023-03-07 17:11:43 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+
+    //Bir class bir classtan ve ya interfaceden miras almıyorsa bir problem çıkar ilerde
+    public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
+ 
